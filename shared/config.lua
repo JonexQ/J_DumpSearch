a60de1e0d9bbf11dc8f0a6796cc7e7eaf0b5f87c
ZoniBoy00 <34036787+ZoniBoy00@users.noreply.github.com>
2024-05-08 15:56:03 +0300

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,6 +1,18 @@
+-- config.lua
 Distance = 2.0 -- Etäisyys
 SearchCooldown = 15 -- Minuuttia
 BleedChance = 5 -- %
 BleedTime = 15 -- Sekuntia
 BleedDamage = 2 -- Damage per sekunti
 TutkimisAika = 3.5 -- Sekuntia
+
+-- ox_lib tekstin vaihto-ominaisuus
+OxLibTextSwap = {
+    ['fi'] = {
+        ['Search Trashcan'] = 'Tongi Roskista',
+        ['Search_Trashcan'] = '[E] Tongi Roskista'
+    }
+}
+
+-- Käytä textui:ta jos ei käytetä ox_target:ia
+UseTextUI = false
